051db3e869d5032427baa4f9b8b45b83b7ee96db
trichellehaight <andrayat@byui.edu>
2019-03-27 11:29:31 -0600

    week 10 updates

---
+++ assignments/lesson10/js/weathersum-preston.js
@@ -0,0 +1,20 @@
+let weatherRequest = new XMLHttpRequest();
+let apiURLstring = "https://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&APPID=db1c5ce9a74473cd07682ac84ee4bbc7";
+weatherRequest.open("Get", apiURLstring, true);
+weatherRequest.send();
+
+weatherRequest.onload = function () {
+
+    let weatherData = JSON.parse(weatherRequest.responseText);
+
+    document.getElementById("current").innerHTML = weatherData.weather[0].description;
+    document.getElementById("high").innerHTML = weatherData.main.temp_max;
+    document.getElementById("humidity").innerHTML = weatherData.main.humidity;
+    document.getElementById("wind").innerHTML = weatherData.wind.speed;
+
+    let speed = parseInt(weatherData.wind.speed, 10);
+    let temp = parseInt(weatherData.main.temp_max, 10);
+
+    return windChill(temp, speed);
+
+}
--- assignments/lesson10/js/windchill.js
+++ assignments/lesson10/js/windchill.js
@@ -1,4 +1,4 @@
-
+function callWindChill() {
     let temp = parseInt(document.getElementById('temp').innerHTML);
     let speed = parseInt(document.getElementById('speed').innerHTML);
     let answer = Math.round(windChill(temp,speed));
@@ -7,4 +7,5 @@
 function windChill(temp, speed) {
     let f = (35.74 + (0.6215 * temp) - (35.75 * Math.pow(speed, 0.16)) + (0.4275 * temp) * (Math.pow(speed, 0.16)));
     return f;
+}
 }
